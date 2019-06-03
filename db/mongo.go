c9c59b9699b62171a3697062f09ca9808a119696
David Morales <daamoralesm@correo.udistrital.edu.co>
2019-06-03 18:19:11 -0500

    rename project

--- db/mongo.go
+++ db/mongo.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/globalsign/mgo"
@@ -8,29 +9,30 @@ import (
 
 // Cursor devuelve un apuntador con la conexión a la bd y la colección especifica
 func Cursor(session *mgo.Session, Collection string) *mgo.Collection {
-	mongoDB := beego.AppConfig.String("mongo_db")
-	c := session.DB(mongoDB).C(Collection)
+	//mongoDB := beego.AppConfig.String("mongo_db")
+	c := session.DB("test").C(Collection)
 	return c
 }
 
 // GetSession crea una sesión con las credenciales
 func GetSession() (*mgo.Session, error) {
 
-	mongoHost := beego.AppConfig.String("mongo_host")
-	mongoUser := beego.AppConfig.String("mongo_user")
-	mongoPassword := beego.AppConfig.String("mongo_pass")
-	mongoDatabase := beego.AppConfig.String("mongo_db_connect")
+	//mongoHost := beego.AppConfig.String("mongo_host")
+	//mongoUser := beego.AppConfig.String("mongo_user")
+	//mongoPassword := beego.AppConfig.String("mongo_pass")
+	//mongoDatabase := beego.AppConfig.String("mongo_db_connect")
 
 	info := &mgo.DialInfo{
-		Addrs:    []string{mongoHost},
+		Addrs:    []string{"financiera_mongo_db"},
 		Timeout:  60 * time.Second,
-		Database: mongoDatabase,
-		Username: mongoUser,
-		Password: mongoPassword,
+		Database: "admin",
+		Username: "test",
+		Password: "test",
 	}
 
 	session, err := mgo.DialWithInfo(info)
 	if err != nil {
+		fmt.Println("Helo this is an error!")
 		panic(err)
 	} else {
 		session.SetMode(mgo.Monotonic, true)
